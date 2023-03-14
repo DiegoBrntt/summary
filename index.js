91c488b3428dba2c5391e84f290f6ebf095a02d7
Diego Brunetti <diego.brntt12@gmail.com>
2023-03-14 10:45:18 -0300

    Desktop View and json load 2

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-import data from "./data.json" assert { type: 'json'};
+fetch("https://diegobrntt.github.io/summary/data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    document.getElementById("circ").innerHTML = Math.round(
+      data.map((data) => data.score).reduce((data1, data2) => data1 + data2) /
+        data.length
+    );
 
-console.log(data);
+    document.getElementById("reactionName").innerHTML = data[0].category;
+    document.getElementById("reactionScore").innerHTML = data[0].score;
+    document.getElementById("reaction100").innerHTML = "/ 100";
+
+    document.getElementById("memoryName").innerHTML = data[1].category;
+    document.getElementById("memoryScore").innerHTML = data[1].score;
+    document.getElementById("memory100").innerHTML = "/ 100";
+
+    document.getElementById("verbalName").innerHTML = data[2].category;
+    document.getElementById("verbalScore").innerHTML = data[2].score;
+    document.getElementById("verbal100").innerHTML = "/ 100";
+
+    document.getElementById("visualName").innerHTML = data[3].category;
+    document.getElementById("visualScore").innerHTML = data[3].score;
+    document.getElementById("visual100").innerHTML = "/ 100";
+  });
